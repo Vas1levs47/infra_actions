6bd121033fb35bdc5ad481c300d84ea68b664292
Vas1levs47 <brynskih91@gmail.com>
2023-05-20 21:22:39 +0500

    12314

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 
 
+
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось!', 200)
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница')
+    return HttpResponse('А это вторая страница', 200)
